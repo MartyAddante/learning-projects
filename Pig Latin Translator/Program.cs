e55b5264b64be2910b6a25cf8327b80a8c30aabc
Martin Addante <martin.addante@gmail.com>
2017-09-04 16:10:07 -0400

    first commit projects

---
+++ Pig Latin Translator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a sentence");
+            string pigRecieve = Console.ReadLine();
+            string[] pigArray = pigRecieve.Split(' ');
+            //string[][] arrayOfPigs = pigArray.Splice('.');
+
+            for(int i = 0; i < pigArray.Length; i++)
+            {
+                if ()
+                {
+
+                }
+                Console.Write(pigArray[i]);
+                Console.Write("ay");
+                Console.Write(" ");
+            }
+            Console.Write(".\n");
+        }
+    }
+}
