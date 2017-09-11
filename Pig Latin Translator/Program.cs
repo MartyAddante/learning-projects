c49f7f5f8862c6ccaf86803721d5310c4da796b8
Martin Addante <martin.addante@gmail.com>
2017-09-11 18:39:40 -0400

    pig Latin translator finished

--- Pig Latin Translator/Program.cs
+++ Pig Latin Translator/Program.cs
@@ -13,17 +13,31 @@ namespace ConsoleApp1
             Console.WriteLine("please enter a sentence");
             string pigRecieve = Console.ReadLine();
             string[] pigArray = pigRecieve.Split(' ');
-            //string[][] arrayOfPigs = pigArray.Splice('.');
+            
+            
 
             for(int i = 0; i < pigArray.Length; i++)
             {
-                if ()
+                pigArray[i] = pigArray[i].Replace('.', " ");
+                string stringPig = pigArray[i].ToLower();
+                
+                if(stringPig[0] == 'a' || stringPig[0] == 'e' || stringPig[0] == 'i' || stringPig[0] == 'o' || stringPig[0] == 'u')
                 {
-
+                    string firstLetter = stringPig.Substring(0, 1);
+                    stringPig = stringPig.Substring(1, stringPig.Length() - 1);
+                    Console.Write(stringPig);
+                    Console.Write("yay");
+                    Console.Write(" ");
                 }
-                Console.Write(pigArray[i]);
-                Console.Write("ay");
-                Console.Write(" ");
+                else
+                {
+                    string firstLetter = stringPig.Substring(0, 1);
+                    stringPig = stringPig.Substring(1, stringPig.Length() - 1);
+                    Console.Write(stringPig);
+                    Console.Write("ay");
+                    Console.Write(" ");
+                }
+                
             }
             Console.Write(".\n");
         }
